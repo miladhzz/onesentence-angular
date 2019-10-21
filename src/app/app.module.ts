2f6fd6a8d012e01e1a13879f2aed9b6bc82ca0c6
miladhzz <miladhzz@gmail.com>
2019-10-21 18:18:52 +0330

    Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddSentenceComponent } from './sentence/components/add/add-sentence/add-sentence.component';
+import { HeaderComponent } from './shared/components/layout/public/header/header.component';
+import { FooterComponent } from './shared/components/layout/public/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddSentenceComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sentence/components/add/add-sentence/add-sentence.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-sentence',
+  templateUrl: './add-sentence.component.html',
+})
+export class AddSentenceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
